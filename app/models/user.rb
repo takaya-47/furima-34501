7260b628624bf2ba17e63de82fc2a580ca5ea732
takaya-47 <local.s.s.47@gmail.com>
2021-02-18 16:00:59 +0900

    モデルのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,20 @@
 class User < ApplicationRecord
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   # email (default)必須、一意性、@を含む
   # password (default)必須、６文字以上、確認用と一致
-  validates :password, format: { with: /[a-z\d]{6}/i, message: 'は半角英数字で入力してください' }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字を含めて入力してください'
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'は全角カタカナで入力してください' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'は全角カタカナで入力してください' }
+    NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+    validates_format_of :last_name, with: NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください'
+    validates_format_of :first_name, with: NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください'
+    NAME_KANA_REGEX = /\A[ァ-ヶ]+\z/
+    validates_format_of :last_name_kana, with: NAME_KANA_REGEX, message: 'は全角カタカナで入力してください'
+    validates_format_of :first_name_kana, with: NAME_KANA_REGEX, message: 'は全角カタカナで入力してください'
     validates :birthday
   end
 
