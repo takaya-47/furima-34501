f3ed169dd3f4443fd6a6608986a5b8b7902aa865
takaya-47 <local.s.s.47@gmail.com>
2021-02-18 10:25:38 +0900

    ユーザー登録のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  # email (default)必須、一意性、@を含む
+  # password (default)必須、６文字以上、確認用と一致
+  validates :nickname, presence: true
+  validates :encrypted_password, format: { with: /[a-z\d]{8}/i, message: "は半角英数字で入力してください" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: "は全角カタカナで入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: "は全角カタカナで入力してください" }
+  validates :birthday, presence: true
+
 end
