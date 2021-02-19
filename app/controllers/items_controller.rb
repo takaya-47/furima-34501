54bd0eb7ce6a098cfe12c2736e6367b7203bfec7
takaya-47 <local.s.s.47@gmail.com>
2021-02-19 11:48:05 +0900

    ルーティング、newアクション、ビューの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
 end
