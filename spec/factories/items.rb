9e801ef743b43b3fe13c10119bb98a74013759de
takaya-47 <local.s.s.47@gmail.com>
2021-02-19 18:10:33 +0900

    ItemモデルのFactoryBotを記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,21 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    explain { "MyText" }
-    price { 1 }
-    user { nil }
+    name { Faker::Name.name }
+    explain{ Faker::Lorem.sentence(word_count: 3) }
+    price { 1000 }
+    category_id { 1 }
+    item_status_id { 1 }
+    shipping_fee_id { 1 }
+    prefecture_id { 1 }
+    delivery_id { 1 }
+
+    # インスタンスに紐づくユーザーも生成
+    association :user
+
+    # インスタンス生成後に画像を生成
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
+
   end
 end
