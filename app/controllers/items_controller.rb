5af3884a3dc063e27313825fbfe5a007b1685593
takaya-47 <local.s.s.47@gmail.com>
2021-02-21 16:51:37 +0900

    他人が出品した商品の編集に対してアクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  # ログインが必要なアクション
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  # 編集は自身が出品した商品しか行えないため、アクセス制限をかける
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: 'DESC').includes(:user)
@@ -42,4 +45,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :explain, :price, :category_id, :item_status_id, :shipping_fee_id,
                                  :prefecture_id, :delivery_id).merge(user_id: current_user.id)
   end
+
+  def ensure_correct_user
+    # URLから変数itemのidを取得する
+    item = Item.find(params[:id])
+    # ログイン中のユーザーのidと、遷移しようとしている商品ページの商品を出品したユーザーのidが一致しなければトップページへ強制的に遷移させる。
+    if current_user.id != item.user.id
+      redirect_to root_path
+    end
+  end
+
 end
