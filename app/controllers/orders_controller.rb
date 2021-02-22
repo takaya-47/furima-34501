625c1bfbe10d1bca04f0bb10cef9884fbf28cf53
takaya-47 <local.s.s.47@gmail.com>
2021-02-22 19:57:08 +0900

    フォームオブジェクトを用いた保存処理、エラーメッセージの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,25 +3,26 @@ class OrdersController < ApplicationController
   def index
     # ルーティングのparamsから商品のidを取得している
     @item = Item.find(params[:item_id])
+    # form_withに渡す、フォームオブジェクトの空のインスタンスを生成
+    @ordershipping = OrderShipping.new
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.save
-      Shipping.create(shipping_params)
+    @ordershipping = OrderShipping.new(ordershipping_params)
+    # フォームオブジェクトクラスはActiveRecordを継承していないのでvalid?を実行してバリデーションを実行する必要がある。
+    if @ordershipping.valid?
+      # バリデーションを通過すればフォームオブジェクトで定義したsaveメソッドを実行
+      @ordershipping.save
       redirect_to root_path
     else
+      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
-    def order_params
-      params.permit(:item_id).merge(user_id: current_user.id)
-    end
-
-    def shipping_params
-      params.permit(:postal_code, :city, :address, :building, :phone_number, :prefecture_id).merge(order_id: @order.id)
+    def ordershipping_params
+      params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id].to_i)
     end
 
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -2,7 +2,8 @@
 class OrderShipping
   include ActiveModel::Model
 
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id
+  # ordersテーブルとshippingsテーブルに保存したいカラムを全て記述
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code , format: { with: /\A[0-9]{3}[-][0-9]{4}\z/ }
@@ -10,7 +11,15 @@ class OrderShipping
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/}
     validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
+    # フォームオブジェクトクラスではアソシエーションを定義できないため、Shippingモデルのbelongs_toと同じバリデーションが必要
     validates :order_id
+  end
+
+  # フォームオブジェクトクラスはActiveRecordを継承していないのでsaveメソッドを自分で記述する必要がある。
+  def save
+    order = Order.create(item_id: params[:item_id].to_i, user_id: current_user.id)
+    # 先に変数orderを作成しておくのがキモ
+    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
 
 end
