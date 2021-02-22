350b0dd6a73927857c19c85d040557f6cf771669
takaya-47 <local.s.s.47@gmail.com>
2021-02-22 17:37:58 +0900

    データベースへの保存処理（フォームオブジェクト導入前）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,27 @@
 class OrdersController < ApplicationController
+
   def index
     # ルーティングのparamsから商品のidを取得している
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      Shipping.create(shipping_params)
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+    def order_params
+      params.permit(:item_id).merge(user_id: current_user.id)
+    end
+
+    def shipping_params
+      params.permit(:postal_code, :city, :address, :building, :phone_number, :prefecture_id).merge(order_id: @order.id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "items#index"
   devise_for :users
   resources :items do
-    resources :orders, only: [:index]
+    resources :orders, only: [:index, :create]
   end
 end
