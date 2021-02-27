b03e98e94ff38b4270367877bc69fa1e42dae4a7
takaya-47 <local.s.s.47@gmail.com>
2021-02-27 10:40:06 +0900

    リンク生成、ビューの生成、再設定メールの送信

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Furima34501
     # 日本語の言語設定を追記する
     config.i18n.default_locale = :ja
 
+    # アプリケーションのホスト情報をメイラー内で使う（Railsガイドより）
+    config.action_mailer.default_url_options = { host: 'example.com' }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
